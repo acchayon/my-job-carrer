8e8cdfbc50653fa72809a59c9f54d909e8c362c1
acchayon <adhikaryac81@gmail.com>
2023-04-11 01:31:48 +0600

    added pie chart

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer, Tooltip } from 'recharts';
+
 
 const Statistics = () => {
+    const chart = useLoaderData();
+    console.log(chart)
+
     return (
-        <div>
-            <h1>Statistics page</h1>
-        </div>
+            <div>
+                <h1 className='text-3xl font-bold text-center'>There is a pie chart about my assignment marks</h1>
+                <PieChart className='w-10/12 mx-auto' width={500} height={500}>
+                <Pie  data={chart} cx={200} cy={200} outerRadius={80} fill="blue" dataKey="marks"></Pie>
+                <Tooltip></Tooltip>
+            </PieChart>
+            </div>
     );
 };
 
