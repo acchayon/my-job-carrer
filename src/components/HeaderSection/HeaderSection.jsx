b213f2b9137f5f9fbb83543bdf595387ef98a9cd
acchayon <adhikaryac81@gmail.com>
2023-04-10 10:18:17 +0600

    added headertitle and section part

---
+++ src/components/HeaderSection/HeaderSection.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import image from '../../assets/1.png'
+
+const HeaderSection = () => {
+    return (
+        <div className='lg:flex justify-evenly mt-10 items-center'>
+            <div className='header-title'>
+                <h1 className='text-6xl font-semibold'>One Step <br /> Closer To Your <br /> <span className='text-amber-400'>Dream Job</span></h1>
+                <p className='mt-5 text-accent-focus font-medium'>Explore thousands of job opportunities with all the <br /> information you need. Its your future. Come find it. Manage all <br /> your job application from start to finish.</p>
+                <button className='btn btn-primary mt-5'>Get Started</button>
+            </div>
+            <div className='header-img'>
+                <img className='w-10/12' src={image} alt="" />
+            </div>
+        </div>
+    );
+};
+
+export default HeaderSection;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import HeaderSection from '../HeaderSection/HeaderSection';
 
 const Home = () => {
     return (
         <div>
-            <h1 className='text-4xl underline'>This is home</h1>
+            {/* <h1 className='text-4xl underline'>This is home</h1> */}
+            <HeaderSection></HeaderSection>
         </div>
     );
 };
